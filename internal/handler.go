a5154d23d555e0de6602f17528e92491b7e5c293
flvsantos15 <flvsantos300@gmail.com>
2024-12-12 18:08:26 -0300

    chore: add handler

---
+++ internal/handler.go
@@ -0,0 +1,33 @@
+package internal
+
+type RouteCreatedEvent struct {
+	EventName  string       `json:"event"`
+	RouteID    string       `json:"id"`
+	Distance   int          `json:"distance"`
+	Directions []Directions `json:"directions"`
+}
+
+type FreightCalculatedEvent struct {
+	EventName string  `json:"event"`
+	RouteID   string  `json:"id"`
+	Amount    float64 `json:"amount"`
+}
+
+func NewFreightCalculatedEvent(routeID string, amount float64) *FreightCalculatedEvent {
+	return &FreightCalculatedEvent{
+		EventName: "FreightCalculated",
+		RouteID:   routeID,
+		Amount:    amount,
+	}
+}
+
+func RouteCreatedHandler(event *RouteCreatedEvent, routeService *RouteService) (*FreightCalculatedEvent, error) {
+	route := NewRoute(event.RouteID, event.Distance, event.Directions)
+	routeCreated, err := routeService.CreateRoute(route)
+	if err != nil {
+		return nil, err
+	}
+
+	freightCalculatedEvent := NewFreightCalculatedEvent(routeCreated.ID, route.FreightPrice)
+	return freightCalculatedEvent, nil
+}
