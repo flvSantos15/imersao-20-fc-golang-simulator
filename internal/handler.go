a5f6e27994f54f96b308d81550e316f018d24a47
flvsantos15 <flvsantos300@gmail.com>
2024-12-12 18:20:25 -0300

    chore: update handler

--- internal/handler.go
+++ internal/handler.go
@@ -7,6 +7,15 @@ type RouteCreatedEvent struct {
 	Directions []Directions `json:"directions"`
 }
 
+func NewRouteCreatedEvent(routeID string, distance int, directions []Directions) *RouteCreatedEvent {
+	return &RouteCreatedEvent{
+		EventName:  "RouteCreated",
+		RouteID:    routeID,
+		Distance:   distance,
+		Directions: directions,
+	}
+}
+
 type FreightCalculatedEvent struct {
 	EventName string  `json:"event"`
 	RouteID   string  `json:"id"`
@@ -18,6 +27,20 @@ func NewFreightCalculatedEvent(routeID string, amount float64) *FreightCalculate
 		EventName: "FreightCalculated",
 		RouteID:   routeID,
 		Amount:    amount,
+	}
+}
+
+// Parei em 1:02:46
+
+type DeliveryStartedEvent struct {
+	EventName string `json:"event"`
+	RouteID   string `json:"route_id"`
+}
+
+func NewDeliveryStartedEvent(routeID string) *DeliveryStartedEvent {
+	return &DeliveryStartedEvent{
+		EventName: "DeliveryStarted",
+		RouteID:   routeID,
 	}
 }
 
